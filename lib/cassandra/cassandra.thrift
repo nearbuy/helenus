b2fa46056f712d375e217a32ccc5538954e3a6a0
Peter Sanford <psanford@retailnext.net>
2017-01-26 14:03:30 -0800

    Update thrift def with local_one consistencylevel

--- lib/cassandra/cassandra.thrift
+++ lib/cassandra/cassandra.thrift
@@ -205,6 +205,7 @@ exception SchemaDisagreementException {
  *   TWO          Ensure that the write has been written to at least 2 node's commit log and memory table
  *   THREE        Ensure that the write has been written to at least 3 node's commit log and memory table
  *   QUORUM       Ensure that the write has been written to <ReplicationFactor> / 2 + 1 nodes
+ *   LOCAL_ONE    Ensure that the write has been written to 1 node within the local datacenter (requires NetworkTopologyStrategy)
  *   LOCAL_QUORUM Ensure that the write has been written to <ReplicationFactor> / 2 + 1 nodes, within the local datacenter (requires NetworkTopologyStrategy)
  *   EACH_QUORUM  Ensure that the write has been written to <ReplicationFactor> / 2 + 1 nodes in each datacenter (requires NetworkTopologyStrategy)
  *   ALL          Ensure that the write is written to <code>&lt;ReplicationFactor&gt;</code> nodes before responding to the client.
@@ -215,6 +216,7 @@ exception SchemaDisagreementException {
  *   TWO          Returns the record with the most recent timestamp once two replicas have replied.
  *   THREE        Returns the record with the most recent timestamp once three replicas have replied.
  *   QUORUM       Returns the record with the most recent timestamp once a majority of replicas have replied.
+ *   LOCAL_ONE    Returns the record with the most recent timestamp once a single replica within the local datacenter have replied.
  *   LOCAL_QUORUM Returns the record with the most recent timestamp once a majority of replicas within the local datacenter have replied.
  *   EACH_QUORUM  Returns the record with the most recent timestamp once a majority of replicas within each datacenter have replied.
  *   ALL          Returns the record with the most recent timestamp once all replicas have replied (implies no replica may be down)..
@@ -228,6 +230,9 @@ enum ConsistencyLevel {
     ANY = 6,
     TWO = 7,
     THREE = 8,
+    SERIAL = 9,
+    LOCAL_SERIAL = 10,
+    LOCAL_ONE = 11,
 }
 
 /**
@@ -242,7 +247,7 @@ struct ColumnParent {
 }
 
 /** The ColumnPath is the path to a single column in Cassandra. It might make sense to think of ColumnPath and
- * ColumnParent in terms of a directory structure.
+ * ColumnParent in terms of a directory structure.s
  *
  * ColumnPath is used to looking up a single column.
  *
